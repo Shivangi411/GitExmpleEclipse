d269e114fe173e63eaffc3b15c21825f9d0a91b4
user <user@DESKTOP-9JT0FUG>
2025-09-05 21:22:47 +0530

    Added Test1.java file

---
+++ GitExmpleEclipse/src/test/java/STAD/GitExmpleEclipse/Test1.java
@@ -0,0 +1,11 @@
+package STAD.GitExmpleEclipse;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub'
+		System.out.println("Hello world!");
+
+	}
+
+}
